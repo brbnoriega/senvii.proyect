bce681d3f43daa49158a2b5d04ede3d80d1f97cf
Sojo506 <fabiansojowork@gmail.com>
2023-02-15 18:53:01 -0600

    add(api/src/(controllers,routes,utils)/(auth/JWT): verification done with Jason Web Token

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
+const { validateToken } = require("../utils/JWT");
 
 const router = new Router();
 
-const { postLogin, postRegister } = require("../controllers/auth");
+const { postLogin, postRegister, getProfile } = require("../controllers/auth");
 
-router.post("/login", postLogin);
+router.route("/login").get(validateToken, getProfile).post(postLogin);
 router.post("/register", postRegister);
 
 module.exports = router;
