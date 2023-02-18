70365ff2f086fc31b389ae6553055ef652022fa4
barbara <91904482+brbnoriega@users.noreply.github.com>
2023-02-18 12:23:34 -0300

    commit accountContext - login - main changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,30 +1,34 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Login from "./component/Login/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 //import Register from './component/Register/Register'
 //import SelectOperation from './component/SelectOperation/SelectOperation'
-import WelcomeBot from './component/WelcomeBot/WelcomeBot'
+import WelcomeBot from "./component/WelcomeBot/WelcomeBot";
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import theme from "./theme";
+import { BrowserRouter } from "react-router-dom";
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-<Routes>
+      <Routes>
 
-
-        <Route path="/login" element={<Login />} />  
-         
-      
-   
-</Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
-    {/*<Register />*/}
-    {/* <SelectOperation /> */}
-    <WelcomeBot />
-  </React.StrictMode>,
-)
+{
+  /*<Register />*/
+}
+{
+  /* <SelectOperation /> */
+}
+//   <WelcomeBot />
+// </React.StrictMode>,
